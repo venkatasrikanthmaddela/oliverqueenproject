ac830b01b598b27f49818e9de7b1143f7086c77b
sidhu140 <sidhu.munagala1@hotmail.com>
2016-08-28 11:24:31 +0530

    Merge branch 'master' of https://github.com/venkatasrikanthmaddela/oliverqueenproject into sidhu_local_Branch
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/pmproject.iml
    #	.idea/workspace.xml

--- adminOperations/urls.py
+++ adminOperations/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import patterns, include,url
+from django.conf.urls import patterns, include, url
 
-from adminOperations.views import AdminUserPage, UploadProjectsPage, ImportProjectsPage,NewProjectIdeaRequestsPage
+from adminOperations.views import AdminUserPage, UploadProjectsPage, ImportProjectsPage, NewProjectIdeaRequestsPage
 
-urlpatterns =  patterns("",
-                        url(r'home',AdminUserPage.as_view(), name="admin-dashboard"),
-                        url(r'upload-projects', UploadProjectsPage.as_view(), name="upload-projects"),
-                        url(r'import-projects', ImportProjectsPage.as_view(), name="import-projects"),
-                        url(r'new-projects-idea-request',NewProjectIdeaRequestsPage.as_view(),name="new-project-idea-requests"),
-                         url(r'delete-new-project-idea-request$',NewProjectIdeaRequestsPage.as_view(), name="admin-dashboard")
-                        )
+urlpatterns = patterns("",
+                       url(r'home', AdminUserPage.as_view(), name="admin-dashboard"),
+                       url(r'upload-projects', UploadProjectsPage.as_view(), name="upload-projects"),
+                       url(r'import-projects', ImportProjectsPage.as_view(), name="import-projects"),
+                       url(r'new-projects-idea-request', NewProjectIdeaRequestsPage.as_view(),
+                           name="new-project-idea-requests"),
+                       url(r'delete-new-project-idea-request$', NewProjectIdeaRequestsPage.as_view(),
+                           name="admin-dashboard")
+                       )
